2d048e15879a43fe5184759583490f406e2f832c
Andrew Wang <44416801+aywang71@users.noreply.github.com>
2024-03-29 14:47:22 -0400

    aux data files added, cannot add main ones because too large

--- IMDB Data Cleaning.R
+++ IMDB Data Cleaning.R
@@ -19,3 +19,19 @@ genres$genres <- NULL
 genres <- genres %>% separate_rows(genre, sep = ",\\s*")
 
 write.csv(genres, "genres.csv")
+
+
+ProductionCompanies <- df %>% select(id, production_companies)
+ProductionCompanies$company <- ProductionCompanies$production_companies
+ProductionCompanies$production_companies <- NULL
+ProductionCompanies <- ProductionCompanies %>% separate_rows(company, sep = ",\\s*")
+
+write.csv(ProductionCompanies, "ProductionCompanies.csv")
+
+
+lang <- df %>% select(id, spoken_languages) 
+lang$language <- lang$spoken_languages
+lang$spoken_languages <- NULL
+lang <- lang %>% separate_rows(language, sep = ",\\s*")
+
+write.csv(lang, "SpokeLanguages.csv")
