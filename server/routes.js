19c84dcd7b35ed441a30902cdfcced55973d1c06
Andrew Wang <44416801+aywang71@users.noreply.github.com>
2024-04-20 12:22:32 -0400

    random endpoint and single filter endpoint

--- server/routes.js
+++ server/routes.js
@@ -13,18 +13,18 @@ const connection = mysql.createConnection({
 connection.connect((err) => err && console.log(err));
 
 // Route 0: GET /example/:params
-const example_route = async function(req, res) {
+const example_route = async function (req, res) {
     connection.query(`
     SELECT COUNT(*) as num
     FROM Movies
   `, (err, data) => {
-    if (err || data.length === 0) {
-      console.log(err);
-      res.json({});
-    } else {
-      res.json({count: data[0].num});
-    }
-  });
+        if (err || data.length === 0) {
+            console.log(err);
+            res.json({});
+        } else {
+            res.json({ count: data[0].num });
+        }
+    });
 }
 
 // Route 1: GET /movie/:movie_id
@@ -69,15 +69,84 @@ const movie = async function (req, res) {
     ORDER BY Movies.popularity DESC
     `;
     connection.query(query, (err, data) => {
+        data[0].id = mid;
         if (err) {
             console.log(err);
-            res.status(500).json({error: "Error querying the database"});
-        } else if ( data.length === 0) {
-            res.status(500).json({error: "No results returned (nonexistent mid)"});
+            res.status(500).json({ error: "Error querying the database" });
+        } else if (data.length === 0) {
+            res.status(500).json({ error: "No results returned (nonexistent mid)" });
         } else {
             res.status(200).json(data);
         }
     });
+}
+
+// Route 6: /random
+
+const random = async function (req, res) {
+    const rand = `SELECT id FROM Movies
+    ORDER BY RAND()
+    LIMIT 1`;
+    connection.query(rand, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.status(500).json({ error: "Error querying the database" });
+        } else {
+            const mid = data[0].id;
+            console.log(mid);
+            const query = `WITH genreID AS (
+                SELECT id, GROUP_CONCAT(genre SEPARATOR ', ') AS genre
+            FROM Genres
+            WHERE id = ${mid}
+            GROUP BY id
+            ),
+            companyID AS (
+                SELECT id, GROUP_CONCAT(company SEPARATOR ', ') AS company
+            FROM ProductionCompanies
+            WHERE id = ${mid}
+            GROUP BY id
+            ),
+            languageID AS (
+                SELECT id, GROUP_CONCAT(language SEPARATOR ', ') AS language
+            FROM SpokenLanguages
+            WHERE id = ${mid}
+            GROUP BY id
+            ),
+            streaming2 AS (
+                SELECT *
+                FROM MovieStreaming NATURAL JOIN Providers
+                ORDER BY display_priority ASC
+            ),
+            streamingID AS (
+                SELECT id, GROUP_CONCAT( DISTINCT provider ORDER BY display_priority SEPARATOR ', ') AS provider, GROUP_CONCAT( DISTINCT provider_logo ORDER BY display_priority SEPARATOR ', ') AS provider_paths
+            FROM streaming2
+            WHERE id = ${mid}
+            GROUP BY id
+            )
+            SELECT *
+            FROM Movies
+            LEFT JOIN genreID ON Movies.id = genreID.id
+            LEFT JOIN companyID ON Movies.id = companyID.id
+            LEFT JOIN languageID ON Movies.id = languageID.id
+            LEFT JOIN streamingID ON Movies.id = streamingID.id
+            WHERE Movies.id = ${mid}
+            ORDER BY Movies.popularity DESC
+            `;
+            connection.query(query, (err, data2) => {
+                
+                data2[0].id = mid;
+                console.log(data2);
+                if (err) {
+                    console.log(err);
+                    res.status(500).json({ error: "Error querying the database" });
+                } else if (data2.length === 0) {
+                    res.status(500).json({ error: "No results returned (nonexistent mid)" });
+                } else {
+                    res.status(200).json(data2);
+                }
+            })
+        }
+    })
 }
 
 // Route 8: /groupSingle/:table_name/:filter_group
@@ -86,9 +155,9 @@ const groupSingle = async function (req, res) { //TODO: Might want to hardcode t
     const filter = req.params.filter_group;
     let query = `select COUNT(*) AS num_movies, AVG(vote_average) AS vote_average, AVG(vote_count) AS vote_count, AVG(revenue) AS avg_revenue, AVG(budget) AS avg_budget, AVG(runtime) AS avg_runtime, AVG(popularity) AS avg_popularity `;
     if (group === "Genres") {
-        query += `from Genres join Movies on Genres.id = Movies.id where genre = '${filter} AND vote_count > 0'`;
+        query += `from Genres join Movies on Genres.id = Movies.id where genre = '${filter}' AND vote_count > 0`;
     } else if (group === "ProductionCompanies") {
-        query += `from ProductionCompanies join Movies on ProductionCompanies.id = Movies.id where company = '${filter} AND vote_count > 0'`;
+        query += `from ProductionCompanies join Movies on ProductionCompanies.id = Movies.id where company = '${filter}' AND vote_count > 0`;
     } else if (group === "SpokenLanguages") {
         query += `from SpokenLanguages join Movies on SpokenLanguages.id = Movies.id where language = '${filter}' AND vote_count > 0`;
     } else if (group === "Providers") { // may need to change the query a bit here
@@ -103,15 +172,18 @@ const groupSingle = async function (req, res) { //TODO: Might want to hardcode t
         WHERE vote_count > 0
         `;
     } else {
-        res.status(500).json({error: "table_name parameter does not exist"});
+        res.status(500).json({ error: "table_name parameter does not exist" });
+        return;
     }
+    //console.log(query);
 
     connection.query(query, (err, data) => {
+        console.log(data[0].num_movies);
         if (err) {
             console.log(err);
-            res.status(500).json({error: "Error querying the database"});
-        } else if ( data.length === 0) {
-            res.status(500).json({error: "No results returned (nonexistent filter))"});
+            res.status(500).json({ error: "Error querying the database" });
+        } else if (data[0].num_movies === 0) {
+            res.status(500).json({ error: "No results returned (nonexistent filter))" });
         } else {
             res.status(200).json(data);
         }
@@ -120,5 +192,8 @@ const groupSingle = async function (req, res) { //TODO: Might want to hardcode t
 
 // Export routes
 module.exports = {
-    example_route
+    example_route,
+    movie,
+    groupSingle,
+    random
 }
