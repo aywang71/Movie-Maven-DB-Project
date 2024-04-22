24e78a1c11f3b3b4f8338d96a80dbd422c320e2e
John-Wesley Appleton <johnwesleyappleton@gmail.com>
2024-04-21 20:45:43 -0400

    cleanup navbar imports

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,9 +1,5 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+import { AppBar, Box, Button, Toolbar, Typography } from '@mui/material';
 import MovieIcon from '@mui/icons-material/Movie';
 import { useNavigate } from 'react-router-dom';
 
