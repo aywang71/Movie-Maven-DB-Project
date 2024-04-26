e854cd21c41a3866c23d3c712d9689f044c915d7
Andrew Wang <44416801+aywang71@users.noreply.github.com>
2024-04-26 13:15:36 -0400

    added type of platform to /movie and /random endpoitns

--- server/routes.js
+++ server/routes.js
@@ -54,10 +54,10 @@ const movie = async function (req, res) {
         ORDER BY display_priority ASC
     ),
     streamingID AS (
-        SELECT id, GROUP_CONCAT( DISTINCT provider ORDER BY display_priority SEPARATOR ', ') AS provider, GROUP_CONCAT( DISTINCT provider_logo ORDER BY display_priority SEPARATOR ', ') AS provider_paths
+        SELECT id, type, GROUP_CONCAT( DISTINCT provider ORDER BY display_priority SEPARATOR ', ') AS provider, GROUP_CONCAT( DISTINCT provider_logo ORDER BY display_priority SEPARATOR ', ') AS provider_paths
     FROM streaming2
     WHERE id = ${mid}
-    GROUP BY id
+    GROUP BY id, type
     )
     SELECT *
     FROM Movies
@@ -320,10 +320,10 @@ const random = async function (req, res) {
                 ORDER BY display_priority ASC
             ),
             streamingID AS (
-                SELECT id, GROUP_CONCAT( DISTINCT provider ORDER BY display_priority SEPARATOR ', ') AS provider, GROUP_CONCAT( DISTINCT provider_logo ORDER BY display_priority SEPARATOR ', ') AS provider_paths
+                SELECT id, type, GROUP_CONCAT( DISTINCT provider ORDER BY display_priority SEPARATOR ', ') AS provider, GROUP_CONCAT( DISTINCT provider_logo ORDER BY display_priority SEPARATOR ', ') AS provider_paths
             FROM streaming2
             WHERE id = ${mid}
-            GROUP BY id
+            GROUP BY id, type
             )
             SELECT *
             FROM Movies
