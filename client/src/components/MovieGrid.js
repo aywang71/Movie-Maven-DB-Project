c40411792ce0c31fd3ef9f436039a53591d22857
John-Wesley Appleton <johnwesleyappleton@gmail.com>
2024-05-02 22:48:50 -0400

    functional browse page

--- client/src/components/MovieGrid.js
+++ client/src/components/MovieGrid.js
@@ -1,36 +1,20 @@
-import * as React from 'react';
-import { Box, Grid, Paper, Typography } from '@mui/material';
+import React from 'react';
+import { Grid, Box } from '@mui/material';
+import { Link } from 'react-router-dom';
 
-const MovieGrid = ({movies}) => {
+const MovieGrid = ({ movies }) => {
   return (
     <Grid container spacing={3}>
       {movies.map(movie => (
         <Grid item key={movie.id} xs={12} sm={6} md={4} lg={3}>
-            <Box position="relative">
-            {/* TODO: `Replace with movie poster. */}
-            <Paper
-              elevation={3}
-              sx={{
-                width: '100%',
-                paddingTop: '150%',
-                position: 'relative'
-              }}
-            >
-              <Typography 
-                variant="subtitle1"
-                sx={{ 
-                  position: 'absolute', 
-                  bottom: 0, 
-                  left: 0, 
-                  width: '100%', 
-                  backgroundColor: '#FFFFF', 
-                  padding: '8px', 
-                  textAlign: 'center' 
-                }}
-              >
-                {movie.title}
-              </Typography>
-            </Paper>
+          <Box>
+            <Link to={`/movieInformation/${movie.id}`}>
+              <img
+                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                alt={movie.title}
+                style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+              />
+            </Link>
           </Box>
         </Grid>
       ))}
