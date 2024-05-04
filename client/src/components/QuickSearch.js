703cb68eec21abbbbb821a70ee14de10d77a9940
Cedric Chan <cedchan@gmail.com>
2024-05-04 05:00:57 -0400

    Make quick search own component

---
+++ client/src/components/QuickSearch.js
@@ -0,0 +1,91 @@
+import { Autocomplete, TextField, Grid, Box, Divider } from "@mui/material";
+import { useState, useMemo } from "react";
+import { debounce } from '@mui/material/utils';
+import { getYear } from "../utils";
+import parse from 'autosuggest-highlight/parse';
+import match from 'autosuggest-highlight/match';
+
+
+const QuickSearch = () => {
+    const [searchValue, setSearchValue] = useState(null);
+    const [searchInputValue, setSearchInputValue] = useState('');
+    const [searchOptions, setSearchOptions] = useState([]);
+
+    // Debounced function to fetch options
+    const fetchOptions = useMemo(() =>
+        debounce((value) => {
+            fetch(`http://localhost:8080/quickSearch/${value}`)
+                .then((resp) => resp.json())
+                .then((data) => {
+                    setSearchOptions(data);
+                });
+        }
+            , 10)
+        , []);
+
+
+    return (
+        <Autocomplete
+            options={searchOptions}
+            getOptionLabel={(option) => option.title}
+            value={searchValue}
+            isOptionEqualToValue={(option, value) => !!option.id}
+            onChange={(event, newValue) => {
+                setSearchValue(newValue);
+                setSearchInputValue('');
+                setSearchOptions([]);
+            }}
+            inputValue={searchInputValue}
+            onInputChange={(event, newInputValue) => {
+                setSearchInputValue(newInputValue);
+                if (newInputValue) {
+                    fetchOptions(newInputValue);
+                } else {
+                    setSearchOptions([]);
+                }
+            }}
+            renderInput={(params) => (
+                <TextField {...params} label="Add a movie" fullWidth />
+            )}
+            renderOption={(props, option, { inputValue }) => {
+                const matches = match(option.title, inputValue, { insideWords: true });
+                const parts = parse(option.title, matches);
+
+                return (
+                    <div key={option.id}>
+                        <li {...props}
+                            key={option.id}
+                        >
+                            <Grid container alignItems="center">
+                                <Grid item sx={{ display: 'flex', width: 44 }}>
+                                    <Box
+                                        component="img"
+                                        src={`https://image.tmdb.org/t/p/original${option?.poster_path}`}
+                                        elevation={3}
+                                        sx={{ width: '80%' }}
+                                    />
+                                </Grid>
+                                <Grid item sx={{ width: 'calc(100% - 44px)', wordWrap: 'break-word' }}>
+                                    {parts.map((part, index) => (
+                                        <span
+                                            key={index}
+                                            style={{
+                                                fontWeight: part.highlight ? 700 : 400,
+                                            }}
+                                        >
+                                            {part.text}
+                                        </span>
+                                    ))}
+                                    {option?.release_date && <i> ({getYear(option.release_date)})</i>}
+                                </Grid>
+                            </Grid>
+                        </li>
+                        {option.id !== searchOptions[searchOptions.length - 1].id && <Divider />}
+                    </div>
+                );
+            }}
+        />
+    );
+};
+
+export default QuickSearch;
--- client/src/pages/MovieRecommendationPage.js
+++ client/src/pages/MovieRecommendationPage.js
@@ -1,94 +1,12 @@
-import { Autocomplete, TextField, Grid, Box } from "@mui/material";
-import { useState, useMemo } from "react";
-import { debounce } from '@mui/material/utils';
-import { getYear } from "../utils";
-import parse from 'autosuggest-highlight/parse';
-import match from 'autosuggest-highlight/match';
 
+import QuickSearch from "../components/QuickSearch";
 
 const MovieRecommendationPage = () => {
-    const [value, setValue] = useState();
-    const [inputValue, setInputValue] = useState('');
-    const [options, setOptions] = useState([]);
-
-    // Debounced function to fetch options
-    const fetchOptions = useMemo(() =>
-        debounce((value) => {
-            fetch(`http://localhost:8080/quickSearch/${value}`)
-                .then((resp) => resp.json())
-                .then((data) => {
-                    setOptions(data);
-                });
-        }
-            , 10)// Adjust debounce delay as needed
-        , []);
-
-
+    
 
     return (
         <>
-            <Autocomplete
-                sx={{ width: 1000 }}
-                freeSolo
-                options={options}
-                getOptionLabel={(option) => option.title}
-                value={value}
-                onChange={(event, newValue) => {
-                    if (typeof newValue === 'object') {
-                        setValue();
-                    }
-                }}
-                inputValue={inputValue}
-                onInputChange={(event, newInputValue) => {
-                    setInputValue(newInputValue);
-                    if (newInputValue) {
-                        fetchOptions(newInputValue);
-                    } else {
-                        setOptions([]);
-                    }
-                }}
-                renderInput={(params) => (
-                    <TextField {...params} label="Add a location" fullWidth />
-                )}
-                renderOption={(props, option, { inputValue }) => {
-                    const matches = match(option.title, inputValue, { insideWords: true });
-                    const parts = parse(option.title, matches);
-
-                    return (
-                        <li {...props}
-                            key={option.id}
-                        >
-                            <Grid container alignItems="center">
-                                <Grid item sx={{ display: 'flex', width: 44 }}>
-                                    <Box
-                                        component="img"
-                                        src={`https://image.tmdb.org/t/p/original${option?.poster_path}`}
-                                        elevation={3}
-                                        sx={{ width: '80%' }}
-                                    />
-                                </Grid>
-                                <Grid item sx={{ width: 'calc(100% - 44px)', wordWrap: 'break-word' }}>
-                                    {parts.map((part, index) => (
-                                        <span
-                                            key={index}
-                                            style={{
-                                                fontWeight: part.highlight ? 700 : 400,
-                                            }}
-                                        >
-                                            {part.text}
-                                        </span>
-                                    ))}
-                                    {option?.release_date && <i> ({getYear(option.release_date)})</i>}
-                                </Grid>
-                            </Grid>
-                        </li>
-
-                    );
-                }}
-            />
-            <p>a</p><p>v</p><p>v</p><p>v</p>
-            Val: {JSON.stringify(value)}
-            Input Val: {inputValue}
+            
         </>
     );
 };
